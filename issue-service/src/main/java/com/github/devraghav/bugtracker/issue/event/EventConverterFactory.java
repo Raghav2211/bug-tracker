653aa5947b92b1596f0d73ea622df566f95d45cf
Raghav2211 <rghvj.22@gmail.com>
2023-01-06 20:20:30 +0530

    fix event factory class in issue-service

--- issue-service/src/main/java/com/github/devraghav/bugtracker/issue/event/EventConverterFactory.java
+++ issue-service/src/main/java/com/github/devraghav/bugtracker/issue/event/EventConverterFactory.java
@@ -2,11 +2,8 @@ package com.github.devraghav.bugtracker.issue.event;
 
 import com.github.devraghav.bugtracker.issue.event.internal.*;
 import com.github.devraghav.data_model.schema.issue.*;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import lombok.Getter;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.springframework.stereotype.Component;
 
@@ -32,86 +29,28 @@ public final class EventConverterFactory {
   private static final IssueCommentUpdatedEventConverter ISSUE_COMMENT_UPDATED_EVENT_CONVERTER =
       new IssueCommentUpdatedEventConverter();
 
-  private static final Map<Class<? extends DomainEvent>, Event> EVENT_SOURCE_MAP =
-      Arrays.stream(Event.values())
-          .sequential()
-          .collect(Collectors.toMap(Event::getSourceClazz, Function.identity()));
+  private static final Map<Class<?>, EventConverter<?, ?>> EVENT_SOURCE_MAP = new HashMap<>();
 
-  @Getter
-  @SuppressWarnings("unchecked")
-  private enum Event {
-    ISSUE_CREATED_EVENT(IssueCreatedEvent.class) {
-      @Override
-      EventConverter<IssueCreatedEvent, IssueCreatedSchema> getConverter() {
-        return ISSUE_CREATED_EVENT_CONVERTER;
-      }
-    },
-    ISSUE_UPDATED_EVENT(IssueUpdatedEvent.class) {
-      @Override
-      EventConverter<IssueUpdatedEvent, IssueUpdatedSchema> getConverter() {
-        return ISSUE_UPDATED_EVENT_CONVERTER;
-      }
-    },
-    ISSUE_ASSIGNED_EVENT(IssueAssignedEvent.class) {
-      @Override
-      EventConverter<IssueAssignedEvent, IssueAssignedSchema> getConverter() {
-        return ISSUE_ASSIGNED_EVENT_CONVERTER;
-      }
-    },
-    ISSUE_RESOLVED_EVENT(IssueResolvedEvent.class) {
-      @Override
-      EventConverter<IssueResolvedEvent, IssueResolvedSchema> getConverter() {
-        return ISSUE_RESOLVED_EVENT_CONVERTER;
-      }
-    },
-    ISSUE_UNASSIGNED_EVENT(IssueUnassignedEvent.class) {
-      @Override
-      EventConverter<IssueUnassignedEvent, IssueUnassignedSchema> getConverter() {
-        return ISSUE_UNASSIGNED_EVENT_CONVERTER;
-      }
-    },
-    ISSUE_WATCH_STARTED_EVENT(IssueWatchStartedEvent.class) {
-      @Override
-      EventConverter<IssueWatchStartedEvent, IssueWatchedSchema> getConverter() {
-        return ISSUE_WATCH_STARTED_EVENT_CONVERTER;
-      }
-    },
-    ISSUE_WATCH_ENDED_EVENT(IssueWatchEndedEvent.class) {
-      @Override
-      EventConverter<IssueWatchEndedEvent, IssueUnwatchedSchema> getConverter() {
-        return ISSUE_WATCH_ENDED_EVENT_CONVERTER;
-      }
-    },
-    ISSUE_COMMENT_CREATED_EVENT(IssueCommentAddedEvent.class) {
-      @Override
-      EventConverter<IssueCommentAddedEvent, CommentAddedSchema> getConverter() {
-        return ISSUE_COMMENT_ADDED_EVENT_CONVERTER;
-      }
-    },
-    ISSUE_COMMENT_UPDATED_EVENT(IssueCommentUpdatedEvent.class) {
-      @Override
-      EventConverter<IssueCommentUpdatedEvent, CommentUpdatedSchema> getConverter() {
-        return ISSUE_COMMENT_UPDATED_EVENT_CONVERTER;
-      }
-    };
-
-    abstract <T extends DomainEvent, R extends SpecificRecordBase>
-        EventConverter<T, R> getConverter();
-
-    final Class<? extends DomainEvent> sourceClazz;
-
-    Event(Class<? extends DomainEvent> sourceClazz) {
-      this.sourceClazz = sourceClazz;
-    }
+  static {
+    EVENT_SOURCE_MAP.put(IssueCreatedEvent.class, ISSUE_CREATED_EVENT_CONVERTER);
+    EVENT_SOURCE_MAP.put(IssueUpdatedEvent.class, ISSUE_UPDATED_EVENT_CONVERTER);
+    EVENT_SOURCE_MAP.put(IssueAssignedEvent.class, ISSUE_ASSIGNED_EVENT_CONVERTER);
+    EVENT_SOURCE_MAP.put(IssueUnassignedEvent.class, ISSUE_UNASSIGNED_EVENT_CONVERTER);
+    EVENT_SOURCE_MAP.put(IssueWatchStartedEvent.class, ISSUE_WATCH_STARTED_EVENT_CONVERTER);
+    EVENT_SOURCE_MAP.put(IssueWatchEndedEvent.class, ISSUE_WATCH_ENDED_EVENT_CONVERTER);
+    EVENT_SOURCE_MAP.put(IssueCommentAddedEvent.class, ISSUE_COMMENT_ADDED_EVENT_CONVERTER);
+    EVENT_SOURCE_MAP.put(IssueCommentUpdatedEvent.class, ISSUE_COMMENT_UPDATED_EVENT_CONVERTER);
+    EVENT_SOURCE_MAP.put(IssueResolvedEvent.class, ISSUE_RESOLVED_EVENT_CONVERTER);
   }
 
+  @SuppressWarnings("unchecked")
   public <T extends DomainEvent, E extends SpecificRecordBase> EventConverter<T, E> getConverter(
       Class<T> sourceClass) {
-    var event = EVENT_SOURCE_MAP.get(sourceClass);
-    if (event == null) {
+    var converter = EVENT_SOURCE_MAP.get(sourceClass);
+    if (converter == null) {
       throw new NullPointerException(
           String.format("No converter found for %s", sourceClass.getName()));
     }
-    return event.getConverter();
+    return (EventConverter<T, E>) converter;
   }
 }
